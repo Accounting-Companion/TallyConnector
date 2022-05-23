726dea624ef96d7dfd42c7f3cacdbf266133632a
Sai Vineeth <64919520+saivineeth100@users.noreply.github.com>
2022-05-23 12:01:17 +0530

    Added TDLXMLSet Attribute class

---
+++ TallyConnector.Core/Attributes/TDLXMLSetAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace TallyConnector.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class TDLXMLSetAttribute : Attribute
+{
+    private string? _set;
+
+    public string? Set
+    {
+        get { return _set; }
+        set { _set = value; }
+    }
+
+}
