a82db91bf4357580114768399193eeaf6051c128
Sai Vineeth Tallapudi <64919520+saivineeth100@users.noreply.github.com>
2023-11-27 12:45:45 +0530

    Added VB.Net Example

---
+++ Examples/TallyConnector_VBDotNetExample/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports TallyConnector.Services
+
+Class MainWindow
+    Dim _tallyService As New TallyConnector.Services.TallyService
+    Public Sub New()
+        'Setting up address
+        _tallyService.Setup("localhost", 9000)
+    End Sub
+
+    Private Async Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim masterTypeStats = Await _tallyService.GetMasterStatisticsAsync()
+        MstStats.ItemsSource = masterTypeStats
+        Dim reqOptions As New TallyConnector.Core.Models.RequestOptions
+        reqOptions.ToDate = TallyService.GetToDate()
+        Dim VchTypeStats = Await _tallyService.GetVoucherStatisticsAsync(reqOptions)
+        Vchtats.ItemsSource = VchTypeStats
+    End Sub
+End Class
