ed0c71c5cf16f8cad9585e9f7f24d136f7aa58e5
Sai Vineeth Tallapudi <64919520+saivineeth100@users.noreply.github.com>
2024-05-06 11:50:54 +0530

    deleted old source generator

--- src/SourceGenerators/TallyConnector.SourceGenerators/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TallyConnector.SourceGenerators;
-public static class Constants
-{
-    public const string SourceGeneratorName = "TallyConnector.SourceGenerators";
-
-    public const string AttrubuteName = "GenerateHelperMethods";
-    public static  string SourceGeneratorVersion = System.Reflection.Assembly.GetAssembly(typeof(Constants)).GetName().Version.ToString();
-
-}
--- src/SourceGenerators/TallyConnector.SourceGenerators/Usings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using static TallyConnector.SourceGenerators.Constants;
-global using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
-global using Microsoft.CodeAnalysis;
-global using Microsoft.CodeAnalysis.CSharp;
-global using Microsoft.CodeAnalysis.CSharp.Syntax;
